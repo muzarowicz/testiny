675e0262dfc73b551f957d1f2d21cf6678f96c71
uzi <m.uzarowicz@piwik.pro>
2025-07-29 10:58:22 +0200

    test_force_fail

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,18 +3,19 @@ import pytest
 # Mapowanie Testiny External ID
 TESTINY_ID = {
     "test_valid_login": "TC_LOGIN_001",
-    "test_invalid_login": "TC_LOGIN_002"
+    "test_invalid_login": "TC_LOGIN_002",
+    "test_force_fail": "TC_LOGIN_003"
 }
 
 @pytest.mark.parametrize("username,password", [("admin", "admin123")])
 def test_valid_login(username, password):
     assert username == "admin" and password == "admin123"
 
-@pytest.mark.parametrize("username,password", [("admin", "wrongpass")])
+@pytest.mark.parametrize("password", [("admin", "wrongpass")])
 def test_invalid_login(password):
     assert password != "admin123"
 
 
-@pytest.mark.parametrize("username,password", [("admin", "wrongpass")])
+@pytest.mark.parametrize("password", [("admin", "wrongpass")])
 def test_force_fail(password):
     assert password == "admin123"
