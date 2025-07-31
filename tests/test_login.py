d45e4bd90f5a13083f256cc5d88dbba18248f354
uzi <m.uzarowicz@piwik.pro>
2025-07-31 08:14:55 +0200

    testinyId

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,26 +1,20 @@
 import pytest
 
-# # Mapowanie Testiny External ID
-# TESTINY_ID = {
-#     "test_valid_login": "TC_LOGIN_001",
-#     "test_invalid_login": "TC_LOGIN_002",
-#     "test_force_fail": "TC_LOGIN_003"
-# }
 
 @pytest.mark.parametrize("username,password", [("admin", "admin123")])
 def test_valid_login(username, password):
-    """[externalId=TC_LOGIN_001]"""
+    """[testinyId=TC-40]"""
     assert username == "admin" and password == "admin123"
 
 @pytest.mark.parametrize("password", ["wrongpass"])
 def test_invalid_login(password):
-    """[externalId=TC_LOGIN_002]"""
+    """[testinyId=TC-41]"""
     assert password != "admin123"
 
 
 @pytest.mark.parametrize("password", ["wrongpass"])
 def test_force_fail(password):
-    """[externalId=TC_LOGIN_003]"""
+    """[testinyId=TC-42]"""
     assert password == "uzipass"
 
 @pytest.mark.parametrize("password", ["uzipass"])
